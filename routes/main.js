bb9b4ae551f9fb4548f26253b15ae11e5590c83c
JaeHee Cho <40957996+cbw1030@users.noreply.github.com>
2020-05-05 20:18:55 +0900

    users.js 함수명 일부 수정/ ID/PW 찾기 구현

--- routes/main.js
+++ routes/main.js
@@ -8,38 +8,45 @@ const db = require('./db');
 /*
     메인 화면을 출력합니다.
 */
-const getMainUi = (req, res) => {
+const getMainPage = (req, res) => {
     let mainStream = '';
-
     mainStream += fs.readFileSync(__dirname + '/../views/header.ejs', 'utf8');
     mainStream += fs.readFileSync(__dirname + '/../views/nav.ejs', 'utf8');
     mainStream += fs.readFileSync(__dirname + '/../views/main.ejs', 'utf8');
     //mainStream += fs.readFileSync(__dirname + '/../views/footer.ejs', 'utf8');
+
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' }); // 200은 성공
 
     // if :로그인된 상태,  else : 로그인안된 상태
-    if (req.session.userId) {  
-        res.end(ejs.render(mainStream,  { title: '세상의 모든 금손, 세모금',
-                                          userName: req.session.who,
-                                          signUpUrl: '/myPage',
-                                          signUpLabel: '마이페이지',
-                                          loginUrl: '/users/logout',
-                                          loginLabel: '로그아웃' }));
-    }
-    else {
-        res.end(ejs.render(mainStream, { title: '세상의 모든 금손, 세모금',
-                                        userName: '비회원',
-                                       signUpUrl: '/users/signUp',
-                                       signUpLabel:'회원가입',
-                                       loginUrl: '/users/login',
-                                       loginLabel: '로그인'}));
+    if (req.session.userId) {
+        res.end(
+            ejs.render(mainStream, {
+                title: '세상의 모든 금손, 세모금',
+                userName: req.session.who,
+                signUpUrl: '/myPage',
+                signUpLabel: '마이페이지',
+                loginUrl: '/users/logout',
+                loginLabel: '로그아웃',
+            })
+        );
+    } else {
+        res.end(
+            ejs.render(mainStream, {
+                title: '세상의 모든 금손, 세모금',
+                userName: '비회원',
+                signUpUrl: '/users/signUp',
+                signUpLabel: '회원가입',
+                loginUrl: '/users/login',
+                loginLabel: '로그인',
+            })
+        );
     }
 };
 
 /*
     공지사항 페이지를 출력합니다.
 */
-const getNotice = (req, res) => {
+const getNoticePage = (req, res) => {
     let noticeStream = '';
     noticeStream += fs.readFileSync(__dirname + '/../views/header.ejs', 'utf8');
     noticeStream += fs.readFileSync(__dirname + '/../views/nav.ejs', 'utf8');
@@ -48,13 +55,30 @@ const getNotice = (req, res) => {
 
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' }); // 200은 성공
 
-    let who = (req.session.userId === undefined) ? '비회원' : req.session.who; 
-    res.end(ejs.render(noticeStream,  { title: '마이페이지',
-                                        userName: who,
-                                        signUpUrl: '/myPage',
-                                        signUpLabel: '마이페이지',
-                                        loginUrl: '/users/logout',
-                                        loginLabel: '로그아웃' }));
+    // if :로그인된 상태,  else : 로그인안된 상태
+    if (req.session.userId) {
+        res.end(
+            ejs.render(noticeStream, {
+                title: '공지사항',
+                userName: req.session.who,
+                signUpUrl: '/myPage',
+                signUpLabel: '마이페이지',
+                loginUrl: '/users/logout',
+                loginLabel: '로그아웃',
+            })
+        );
+    } else {
+        res.end(
+            ejs.render(noticeStream, {
+                title: '공지사항',
+                userName: '비회원',
+                signUpUrl: '/users/signUp',
+                signUpLabel: '회원가입',
+                loginUrl: '/users/login',
+                loginLabel: '로그인',
+            })
+        );
+    }
 };
 
 /*
@@ -63,7 +87,7 @@ const getNotice = (req, res) => {
 const getMyPage = (req, res) => {
     let myPageStream = '';
     let myPageErrorStream = '';
-    
+
     myPageStream += fs.readFileSync(__dirname + '/../views/header.ejs', 'utf8');
     myPageStream += fs.readFileSync(__dirname + '/../views/nav.ejs', 'utf8');
     myPageStream += fs.readFileSync(__dirname + '/../views/myPage.ejs', 'utf8');
@@ -74,25 +98,63 @@ const getMyPage = (req, res) => {
     //myPageErrorStream += fs.readFileSync(__dirname + '/../views/footer.ejs', 'utf8');
 
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' }); // 200은 성공
-    
-    if (req.session.userId) {  
-        res.end(ejs.render(myPageStream,  { title: '마이페이지',
-                                          userName: req.session.who,
-                                          signUpUrl: '/myPage',
-                                          signUpLabel: '마이페이지',
-                                          loginUrl: '/users/logout',
-                                          loginLabel: '로그아웃' }));
+
+    // if :로그인된 상태,  else : 로그인안된 상태
+    if (req.session.userId) {
+        res.end(
+            ejs.render(myPageStream, {
+                title: '마이페이지',
+                userName: req.session.who,
+                signUpUrl: '/myPage',
+                signUpLabel: '마이페이지',
+                loginUrl: '/users/logout',
+                loginLabel: '로그아웃',
+            })
+        );
+    } else {
+        res.end(ejs.render(myPageErrorStream, { title: '에러 페이지', errorMessage: '로그인이 필요합니다.' }));
     }
-    else {
-        res.end(ejs.render(myPageErrorStream, { title: '에러 페이지',
-                                        errorMessage: '로그인이 필요합니다.'}));
+};
+
+/*
+    장바구니 페이지를 출력합니다.
+*/
+const getCartPage = (req, res) => {
+    let cartStream = '';
+    let cartErrorStream = '';
+
+    cartStream += fs.readFileSync(__dirname + '/../views/header.ejs', 'utf8');
+    cartStream += fs.readFileSync(__dirname + '/../views/nav.ejs', 'utf8');
+    cartStream += fs.readFileSync(__dirname + '/../views/cart.ejs', 'utf8');
+    //cartStream += fs.readFileSync(__dirname + '/../views/footer.ejs', 'utf8');
+
+    cartErrorStream += fs.readFileSync(__dirname + '/../views/header.ejs', 'utf8');
+    cartErrorStream += fs.readFileSync(__dirname + '/../views/error.ejs', 'utf8');
+    //cartErrorStream += fs.readFileSync(__dirname + '/../views/footer.ejs', 'utf8');
+
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' }); // 200은 성공
+
+    // if :로그인된 상태,  else : 로그인안된 상태
+    if (req.session.userId) {
+        res.end(
+            ejs.render(cartStream, {
+                title: '장바구니',
+                userName: req.session.who,
+                signUpUrl: '/myPage',
+                signUpLabel: '마이페이지',
+                loginUrl: '/users/logout',
+                loginLabel: '로그아웃',
+            })
+        );
+    } else {
+        res.end(ejs.render(cartErrorStream, { title: '에러 페이지', errorMessage: '로그인이 필요합니다.' }));
     }
 };
 
 /*
     한 모금, 투표하기 페이지를 출력합니다.
 */
-const getOneMogeum = (req, res) => {
+const getOneMogeumPage = (req, res) => {
     let oneMogeumStream = '';
     oneMogeumStream += fs.readFileSync(__dirname + '/../views/header.ejs', 'utf8');
     oneMogeumStream += fs.readFileSync(__dirname + '/../views/nav.ejs', 'utf8');
@@ -100,30 +162,37 @@ const getOneMogeum = (req, res) => {
     //oneMogeumStream += fs.readFileSync(__dirname + '/../views/footer.ejs', 'utf8');
 
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' }); // 200은 성공
-    
+
     // if :로그인된 상태,  else : 로그인안된 상태
-    if (req.session.userId) {  
-        res.end(ejs.render(oneMogeumStream,  { title: '한 모금, 투표하기',
-                                        userName: req.session.who,
-                                        signUpUrl: '/myPage',
-                                        signUpLabel: '마이페이지',
-                                        loginUrl: '/users/logout',
-                                        loginLabel: '로그아웃' }));
-    }
-    else {
-        res.end(ejs.render(oneMogeumStream, { title : '한 모금, 투표하기',
-                                        userName: '비회원',
-                                        signUpUrl: '/users/signUp',
-                                        signUpLabel:'회원가입',
-                                        loginUrl: '/users/login',
-                                        loginLabel: '로그인'}));
+    if (req.session.userId) {
+        res.end(
+            ejs.render(oneMogeumStream, {
+                title: '한 모금, 투표하기',
+                userName: req.session.who,
+                signUpUrl: '/myPage',
+                signUpLabel: '마이페이지',
+                loginUrl: '/users/logout',
+                loginLabel: '로그아웃',
+            })
+        );
+    } else {
+        res.end(
+            ejs.render(oneMogeumStream, {
+                title: '한 모금, 투표하기',
+                userName: '비회원',
+                signUpUrl: '/users/signUp',
+                signUpLabel: '회원가입',
+                loginUrl: '/users/login',
+                loginLabel: '로그인',
+            })
+        );
     }
 };
 
 /*
     두 모금, 선정결과 페이지를 출력합니다.
 */
-const getTwoMogeum = (req, res) => {
+const getTwoMogeumPage = (req, res) => {
     let twoMogeumStream = '';
     twoMogeumStream += fs.readFileSync(__dirname + '/../views/header.ejs', 'utf8');
     twoMogeumStream += fs.readFileSync(__dirname + '/../views/nav.ejs', 'utf8');
@@ -133,28 +202,35 @@ const getTwoMogeum = (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' }); // 200은 성공
 
     // if :로그인된 상태,  else : 로그인안된 상태
-    if (req.session.userId) {  
-        res.end(ejs.render(twoMogeumStream,  { title: '두 모금, 투표하기',
-                                        userName: req.session.who,
-                                        signUpUrl: '/myPage',
-                                        signUpLabel: '마이페이지',
-                                        loginUrl: '/users/logout',
-                                        loginLabel: '로그아웃' }));
-    }
-    else {
-        res.end(ejs.render(twoMogeumStream, { title : '두 모금, 투표하기',
-                                        userName: '비회원',
-                                        signUpUrl: '/users/signUp',
-                                        signUpLabel:'회원가입',
-                                        loginUrl: '/users/login',
-                                        loginLabel: '로그인'}));
+    if (req.session.userId) {
+        res.end(
+            ejs.render(twoMogeumStream, {
+                title: '두 모금, 투표하기',
+                userName: req.session.who,
+                signUpUrl: '/myPage',
+                signUpLabel: '마이페이지',
+                loginUrl: '/users/logout',
+                loginLabel: '로그아웃',
+            })
+        );
+    } else {
+        res.end(
+            ejs.render(twoMogeumStream, {
+                title: '두 모금, 투표하기',
+                userName: '비회원',
+                signUpUrl: '/users/signUp',
+                signUpLabel: '회원가입',
+                loginUrl: '/users/login',
+                loginLabel: '로그인',
+            })
+        );
     }
 };
 
 /*
     세 모금, 구매하기 페이지를 출력합니다.
 */
-const getThreeMogeum = (req, res) => {
+const getThreeMogeumPage = (req, res) => {
     let threeMogeumStream = '';
     threeMogeumStream += fs.readFileSync(__dirname + '/../views/header.ejs', 'utf8');
     threeMogeumStream += fs.readFileSync(__dirname + '/../views/nav.ejs', 'utf8');
@@ -164,30 +240,37 @@ const getThreeMogeum = (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' }); // 200은 성공
 
     // if :로그인된 상태,  else : 로그인안된 상태
-    if (req.session.userId) {  
-        res.end(ejs.render(threeMogeumStream,  { title: '세 모금, 투표하기',
-                                        userName: req.session.who,
-                                        signUpUrl: '/myPage',
-                                        signUpLabel: '마이페이지',
-                                        loginUrl: '/users/logout',
-                                        loginLabel: '로그아웃' }));
-    }
-    else {
-        res.end(ejs.render(threeMogeumStream, { title : '세 모금, 투표하기',
-                                        userName: '비회원',
-                                        signUpUrl: '/users/signUp',
-                                        signUpLabel:'회원가입',
-                                        loginUrl: '/users/login',
-                                        loginLabel: '로그인'}));
+    if (req.session.userId) {
+        res.end(
+            ejs.render(threeMogeumStream, {
+                title: '세 모금, 투표하기',
+                userName: req.session.who,
+                signUpUrl: '/myPage',
+                signUpLabel: '마이페이지',
+                loginUrl: '/users/logout',
+                loginLabel: '로그아웃',
+            })
+        );
+    } else {
+        res.end(
+            ejs.render(threeMogeumStream, {
+                title: '세 모금, 투표하기',
+                userName: '비회원',
+                signUpUrl: '/users/signUp',
+                signUpLabel: '회원가입',
+                loginUrl: '/users/login',
+                loginLabel: '로그인',
+            })
+        );
     }
 };
 
-router.get('/', getMainUi);
-router.get('/notice', getNotice);
+router.get('/', getMainPage);
+router.get('/notice', getNoticePage);
 router.get('/myPage', getMyPage);
-router.get('/oneMogeum', getOneMogeum);
-router.get('/twoMogeum', getTwoMogeum);
-router.get('/threeMogeum', getThreeMogeum);
-
+router.get('/cart', getCartPage);
+router.get('/oneMogeum', getOneMogeumPage);
+router.get('/twoMogeum', getTwoMogeumPage);
+router.get('/threeMogeum', getThreeMogeumPage);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,11 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const router = express.Router();
+const   methodOverride = require('method-override');
 /* DB 연동 모듈 불러옴 */
 const db = require('./db');
+
+router.use(methodOverride('_method')); // put을 사용하기 위함
 
 /*********************************************************** */
 /************************ 회원가입 기능 ***********************/
@@ -32,10 +35,11 @@ const getSignUpPage = (req, res) => {
 */
 const handleSignUp = (req, res) => {
     let str1 = 'SELECT * FROM USER WHERE userId=?';
-    let str2 = 'INSERT INTO USER(userId, userName, userPwd, userPwdConfirm) VALUES(?, ?, ?, ?)';
+    let str2 = 'INSERT INTO USER(userId, userName, userPhone, userPwd, userPwdConfirm) VALUES(?, ?, ?, ?, ?)';
     let body = req.body;
     let userId = body.userId;
     let userName = body.userName;
+    let userPhone = body.userPhone;
     let userPwd = body.userPwd;
     let userPwdConfirm = body.userPwdConfirm;
 
@@ -49,7 +53,7 @@ const handleSignUp = (req, res) => {
         } else {
             // 입력받은 데이터가 DB에 존재하는지 판단합니다.
             if (results[0] == undefined && userPwd == userPwdConfirm) {
-                db.query(str2, [userId, userName, userPwd, userPwdConfirm], (error) => {
+                db.query(str2, [userId, userName, userPhone, userPwd, userPwdConfirm], (error) => {
                     if (error) {
                         res.end('error');
                         console.log(error);
@@ -101,6 +105,7 @@ const getLoginPage = (req, res) => {
 */
 const handleLogin = (req, res) => {
     let body = req.body; // req.body에 login.ejs 폼으로부터 name값 value값이 객체 형식으로 넘어옴 {uid: '어쩌고', pass: '저쩌고'}
+    console.log('body: ', body);
     let userId, userPwd, userName;
     let str1;
     let handleLoginErrorStream = '';
@@ -135,7 +140,7 @@ const handleLogin = (req, res) => {
                     res.status(562).end(
                         ejs.render(handleLoginErrorStream, {
                             title: '로그인 에러',
-                            errorMessage: '등록된 계정이 없습니다.',
+                            errorMessage: '아이디 또는 비밀번호가 일치하지 않습니다.',
                         })
                     );
                     console.log('등록된 아이디가 존재하지 않습니다.');
@@ -177,13 +182,186 @@ const handleLogout = (req, res) => {
     res.redirect('/'); // 로그아웃후 메인화면으로 재접속
 };
 
+/*********************************************************** */
+/************************* 아이디 찾기 ************************/
+/*********************************************************** */
+/*
+    아이디 찾기 페이지 출력합니다.
+*/
+const getFindIdPage = (req, res) => {
+    let findIdStream = '';
+    findIdStream += fs.readFileSync(__dirname + '/../views/header.ejs', 'utf8');
+    findIdStream += fs.readFileSync(__dirname + '/../views/findId.ejs', 'utf8');
+    //findIdStream += fs.readFileSync(__dirname + '/../views/footer.ejs', 'utf8');
 
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' }); // 200은 성공
+    res.end(
+        ejs.render(findIdStream, {
+            title: '아이디 찾기',
+        })
+    );
+};
+
+/*
+    아이디 찾기를 처리합니다.
+*/
+const handleFindId = (req, res) => {
+    let str1 = 'SELECT userId FROM USER WHERE userName=? AND userPhone=?';
+    let body = req.body;
+    let userName = body.userName;
+    let userPhone = body.userPhone;
+
+    let findIdResultStream = '';
+    let errorStream = '';
+
+    db.query(str1, [userPhone, userName], (error, results) => {
+        if (error) {
+            console.log(error);
+            res.end('error');
+        } else {
+            // 입력받은 데이터가 DB에 존재하는지 판단합니다.
+            if (results[0] == null) {
+                errorStream += fs.readFileSync(__dirname + '/../views/header.ejs', 'utf8');
+                errorStream += fs.readFileSync(__dirname + '/../views/error.ejs', 'utf8');
+                //errorStream += fs.readFileSync(__dirname + '/../views/footer.ejs','utf8');
+
+                res.status(562).end(
+                    ejs.render(errorStream, {
+                        title: '에러 페이지',
+                        errorMessage: '아이디 찾기를 처리하는 도중 에러가 발생했습니다.',
+                    })
+                );
+            } else {
+                findIdResultStream += fs.readFileSync(__dirname + '/../views/header.ejs', 'utf8');
+                findIdResultStream += fs.readFileSync(__dirname + '/../views/findIdResult.ejs', 'utf8');
+                //findIdResultStream += fs.readFileSync(__dirname + '/../views/footer.ejs', 'utf8');
+
+                res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' }); // 200은 성공
+                res.end(
+                    ejs.render(findIdResultStream, {
+                        title: '아이디 찾기 결과',
+                        userId: results[0].userId,
+                    })
+                );
+            }
+        }
+    });
+};
+
+/*********************************************************** */
+/************************ 비밀번호 찾기 ***********************/
+/*********************************************************** */
+/*
+    비밀번호 찾기 페이지 출력합니다.
+*/
+const getFindPwdPage = (req, res) => {
+    let findPwdStream = '';
+    findPwdStream += fs.readFileSync(__dirname + '/../views/header.ejs', 'utf8');
+    findPwdStream += fs.readFileSync(__dirname + '/../views/findPwd.ejs', 'utf8');
+    //findPwdStream += fs.readFileSync(__dirname + '/../views/footer.ejs', 'utf8');
+
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' }); // 200은 성공
+
+    res.end(
+        ejs.render(findPwdStream, {
+            title: '비밀번호 찾기',
+        })
+    );
+};
+
+/*
+    비밀번호 변경 페이지를 출력합니다. 
+*/
+// 새로운 비밀번호를 설정하기 위해 데이터를 입력 즉시 변경 페이지로 이동합니다.
+const getChangePwdPage = (req, res) => {
+    console.log("비밀번호 변경 POST 요청 보냄");
+    let str1 = 'SELECT * FROM USER WHERE userId=? AND userPhone=?';
+    let body = req.body;
+    let userId = body.userId;
+    let userPhone = body.userPhone;
+
+    let pwdChangeResultStream = '';
+    let errorStream = '';
+
+    db.query(str1, [userId, userPhone], (error, results) => {
+        if (error) {
+            console.log(error);
+            res.end('error');
+        } else {
+            // 입력받은 데이터가 DB에 존재하는지 판단합니다.
+            if (results[0] == null) {
+                errorStream += fs.readFileSync(__dirname + '/../views/header.ejs', 'utf8');
+                errorStream += fs.readFileSync(__dirname + '/../views/error.ejs', 'utf8');
+                //errorStream += fs.readFileSync(__dirname + '/../views/footer.ejs', 'utf8');
+
+                res.status(562).end(
+                    ejs.render(errorStream, {
+                        title: '에러 페이지',
+                        errorMessage: '비밀번호 변경 페이지를 출력하는 도중 에러가 발생했습니다.',
+                    })
+                );
+            } else {
+                pwdChangeResultStream += fs.readFileSync(__dirname + '/../views/header.ejs', 'utf8');
+                pwdChangeResultStream += fs.readFileSync(__dirname + '/../views/changePwd.ejs', 'utf8');
+                //pwdChangeResultStream += fs.readFileSync(__dirname + '/../views/footer.ejs', 'utf8');
+
+                res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' }); // 200은 성공
+
+                res.end(
+                    ejs.render(pwdChangeResultStream, {
+                        title: '비밀번호 변경',
+                        userId: userId,
+                    })
+                );
+            }
+        }
+    });
+};
+
+/*
+    비밀번호 변경을 처리합니다.
+*/
+const handleChangePwd = (req, res) => {
+    console.log("비밀번호 변경 PUT 요청 보냄");
+    let str1 = 'UPDATE USER SET userPwd=? WHERE userId=?';
+    let body = req.body;
+    let userId = body.userId;
+    let userPwd = body.userPwd;
+
+    let errorStream = '';
+
+    console.log(body);
+    db.query(str1, [userPwd, userId], (error, results) => {
+        if (error) {
+            console.log(error);
+            errorStream += fs.readFileSync(__dirname + '/../views/header.ejs', 'utf8');
+            errorStream += fs.readFileSync(__dirname + '/../views/error.ejs', 'utf8');
+            //errorStream += fs.readFileSync(__dirname + '/../views/footer.ejs','utf8');
+            res.status(562).end(
+                ejs.render(errorStream, {
+                    title: '에러 페이지',
+                    error: '비밀번호 변경을 처리하는 도중 에러가 발생했습니다.',
+                })
+            );
+        } else {
+            // 테스트 코드
+            console.log(results);
+            res.redirect('/users/login');
+        }
+    });
+};
 
 router.get('/signUp', getSignUpPage);
 router.get('/login', getLoginPage);
 router.get('/logout', handleLogout);
+router.get('/findId', getFindIdPage);
+router.get('/findPwd', getFindPwdPage);
 
 router.post('/signUp', handleSignUp);
 router.post('/login', handleLogin);
+router.post('/findId', handleFindId);
+router.post('/findPwd', getChangePwdPage);
+
+router.put('/changePwd', handleChangePwd);
 
 module.exports = router;
