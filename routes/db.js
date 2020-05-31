54994edd255f8c86ad27a4eda6578499d5c05f94
Jiyoun Kim <prinyun11@naver.com>
2020-06-01 01:20:49 +0900

    투표 관련 조항 추가

--- routes/db.js
+++ routes/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: 'localhost',
     port: 3306, // DB서버 IP주소
-    user: 'root', // DB접속 아이디
-    password: 'root', // 계정암호
+    user: 'Anna', // DB접속 아이디
+    password: 'Rlawldbs98!', // 계정암호
     database: 'semogeum', //사용할 DB명
     charset: 'utf8',
     multipleStatements: true,
